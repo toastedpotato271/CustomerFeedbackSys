466c01ecb61247cc04d75134f92b2ab275033b1e
toastedpotato271 <aaron.cruz@ciit.edu.ph>
2024-11-26 13:46:22 +0800

    fc

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -7,13 +7,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAo2INi_uaBDofD9-KrxrkEGlevMrnCJ40",
-  authDomain: "cfs-react-4c4eb.firebaseapp.com",
-  projectId: "cfs-react-4c4eb",
-  storageBucket: "cfs-react-4c4eb.firebasestorage.app",
-  messagingSenderId: "657323064458",
-  appId: "1:657323064458:web:464d1eb9c8b76e2b2f0173",
-  measurementId: "G-SSGTZE11R3"
+  apiKey: "your-api-key",
+  authDomain: "your-auth-domain",
+  projectId: "your-project-id",
+  storageBucket: "your-storage-bucket",
+  messagingSenderId: "your-messaging-sender-id",
+  appId: "your-app-id",
 };
 
 // Initialize Firebase
